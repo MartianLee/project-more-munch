026d97978c84bf81300a0cf7615e6ede835cdd9c
MartianLee <martionlee@gmail.com>
2026-03-18 03:26:20 +0900

    feat: add Settings module (GET/PATCH /settings)

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaModule } from './prisma/prisma.module';
+import { SettingsModule } from './settings/settings.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     ScheduleModule.forRoot(),
     PrismaModule,
+    SettingsModule,
   ],
 })
 export class AppModule {}
